3b4c6987093860c3a2ae227182a0ebd22f301bc2
hiriethart <mxh9219@rit.edu>
2018-02-19 21:04:59 -0500

    project 1 and clock exercise

---
+++ clock-exercise/scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (GameObject.Find("ClockHand").GetComponent<RotateHand>().enabled == false)
+        {
+            GameObject.Find("ClockHand").GetComponent<RotateHand>().enabled = true;
+        }
+        else
+        {
+            GameObject.Find("ClockHand").GetComponent<RotateHand>().enabled = false;
+        }               
+
+    }
+}
